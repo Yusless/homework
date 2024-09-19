ac18c3b7762685d02efa9ff2d009fb3414942792
yusless <13yusa2007@mail.ru>
2024-09-19 19:04:14 +0300

    modified:   2nd week/#3.py 	modified:   2nd week/#4.py

--- 2nd week/#4.py
+++ 2nd week/#4.py
@@ -1,13 +1,10 @@
 #4
 d=0
-w=0
+m=0
 a=list(map(int, input().split()))
 for i in range (len(a)):
-    s=0
-    for k in range(len(a)):
-        if a[i] == a[k]:
-            s+=1
-    if w<s:
-        w=s
-        d=a[i]
-print(d)
+    s=a.count(a[i])
+    if d<s:
+        d=s
+        m=a[i]
+print(m)
